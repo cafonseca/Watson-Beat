079afb4397c9c48a3c626e1e78e5e5af8269d7ce
Carlos Fonseca <cafonseca@us.ibm.com>
2018-11-11 11:27:18 -0500

    Initial Commit

---
+++ Watson-Beat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Watson-Beat
+//
+//  Created by fonseca on 11/11/18.
+//  Copyright © 2018 IBM Research. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Watson-Beat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Watson-Beat
+//
+//  Created by fonseca on 11/11/18.
+//  Copyright © 2018 IBM Research. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
